b830a6f686d205493b570a53ba7b943c8ff7f2d0
QuanDoan911 <quan.doan@logigear.com>
2021-03-27 11:17:34 +0700

    Update path and get test case name to run test case

--- src/test/java/Vietjet/testcases/BaseTest.java
+++ src/test/java/Vietjet/testcases/BaseTest.java
@@ -8,6 +8,7 @@ import utilities.BrowserSettingHelper;
 import utilities.JsonHelper;
 import utilities.PropertiesHelper;
 
+import java.io.File;
 import java.io.IOException;
 
 import static utilities.GlobalVariables.*;
@@ -15,42 +16,74 @@ import static utilities.GlobalVariables.*;
 public class BaseTest {
 
     public String testCaseName;
-
-    //Initiate variable for log4j
     public static PropertiesHelper propertiesHelper = null;
 
-    @BeforeSuite()
+    @BeforeSuite
     public void beforeSuite() {
-
-        // Initial test data
+        System.out.println("beforeSuite");
         propertiesHelper = new PropertiesHelper(TEST_CONFIGURATION);
 
-        // Validate wait time
+        // Validate waitTime
         SHORT_TIMEOUT = Integer.parseInt(propertiesHelper.getDataFromConfigurationFile("Short_Timeout"));
         MEDIUM_TIMEOUT = Integer.parseInt(propertiesHelper.getDataFromConfigurationFile("Medium_Timeout"));
         LONG_TIMEOUT = Integer.parseInt(propertiesHelper.getDataFromConfigurationFile("Long_Timeout"));
 
         // Validate testingType
-        TESTING_TYPE = propertiesHelper.getDataFromConfigurationFile("TestingType");
+        TESTING_TYPE = propertiesHelper.getDataFromConfigurationFile("TestingType").toLowerCase();
 
         // Validate environment
-        ENVIRONMENT = propertiesHelper.getDataFromConfigurationFile("Environment");
-    }
-    @BeforeClass
-    public synchronized void beforeClass(){
+        ENVIRONMENT = propertiesHelper.getDataFromConfigurationFile("Environment").toLowerCase();
+
+        // Validate browserName
+        BROWSER = propertiesHelper.getDataFromConfigurationFile("BrowserName").toLowerCase();
+
+        // Validate runOn
+        RUN_ON = propertiesHelper.getDataFromConfigurationFile("RunOn").toLowerCase();
     }
 
-    @BeforeMethod(alwaysRun = true)
-    @Parameters("browser")
-    public void beforeMethod(@Optional String browser) throws IllegalAccessException {
-        DriverProperty property = BrowserSettingHelper.getDriverProperty("browser");
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("beforeTest");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("beforeClass");
+        testCaseName = this.getClass().getSimpleName();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() throws IllegalAccessException {
+        System.out.println("beforeMethod");
+        DriverProperty property = BrowserSettingHelper.getDriverProperty(BROWSER+"."+RUN_ON);
         DriverManagerFactory.getDriverManager(property).setWebDriver();
         DriverUtilities.maximizeBrowser();
     }
 
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("afterMethod");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("afterClass");
+        DriverUtilities.closeBrowser();
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("afterTest");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("afterSuite");
+    }
+
     @DataProvider
     public Object[][] getDataForTest() throws IOException {
-        String DataFilePath = TEST_DATA_JSON + "/" + this.getClass().getPackage().getName().replace(".", "/") + "/data.json";
+        String DataFilePath = TEST_DATA_JSON  + File.separator +  this.getClass().getPackage().getName().replace(".", File.separator)  + File.separator + "data.json";
         Object[][] data = JsonHelper.getData(testCaseName, DataFilePath);
         return data;
     }
--- src/test/java/Vietjet/testcases/VJ01_SearchAndChooseTicketsOnASpecificDaySuccessfully.java
+++ src/test/java/Vietjet/testcases/VJ01_SearchAndChooseTicketsOnASpecificDaySuccessfully.java
@@ -3,9 +3,16 @@ package Vietjet.testcases;
 import org.testng.annotations.Test;
 import java.util.Hashtable;
 
+import static utilities.GlobalVariables.SHORT_TIMEOUT;
+
 public class VJ01_SearchAndChooseTicketsOnASpecificDaySuccessfully extends BaseTest{
     @Test(dataProvider = "getDataForTest", priority = 1, description = "Search and choose tickets on a specific day successfully")
     public void TC01(Hashtable<String, String> data) {
-
+        try {
+            System.out.println("Test");
+            Thread.sleep(SHORT_TIMEOUT*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
